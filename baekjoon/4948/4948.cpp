3f75fd4515a0e5197ba3c4e9da6013438ee64eb7
Sang-Min Kang <rdrniel12345@gmail.com>
2022-08-27 04:28:38 +0900

    4948 time out code

--- baekjoon/4948/4948.cpp
+++ baekjoon/4948/4948.cpp
@@ -2,37 +2,38 @@
 #include <string>
 using namespace std;
 
-const int SIZE = 1e6 + 1;
+const int SIZE = 123456 * 2 + 1;
 bool nums[SIZE] = {true, true};
-
-void seize(int x) {
-    if(nums[x])
-        return;
-    for(int i = 2; i * x < SIZE; i++) {
-        nums[i * x] = true;
-    }
-    return;
-}
+int p[SIZE] = {2, 2};
 
 int main() {
-    int cases;
-    string result = "";
+    int idx = 2;
+    for(int i = 2; i < SIZE; i++) {
+        if(!nums[i]) {
+            for(int j = 2; j * i < SIZE; j++) {
+                nums[j * i] = true;
+            }
+            p[idx] = i;
+            idx = i;
+        }
+    }
 
+    int input;
+    string result = "";
     while(true) {
-        cin >> cases;
-        if(cases == 0)
+        cin >> input;
+        if(input == 0)
             break;
-        
+
         int cnt = 0;
-        for(int i = 2; i <= cases * 2; i++) {
-            if(i * i <= cases * 2 && !nums[i])
-                seize(i);
-            if(i > cases && !nums[i])
-                cnt++;
+        int pos = 2;
+        while(pos <= input * 2) {
+            if(pos > input) 
+                cnt ++;
+            pos = p[pos];
         }
         result += to_string(cnt) + "\n";
     }
-
     cout << result;
 
     return 0;
